57a01e06aa22ca83ff171bef0a30a629f9d7f757
adrianKaPunkt <adriank@gmx.de>
2025-04-09 16:58:10 +0200

    grace fix3

--- app/(home)/_sections/Dia.tsx
+++ app/(home)/_sections/Dia.tsx
@@ -2,7 +2,6 @@ import DiaIntro from '../_components/dia/DiaIntro';
 import DreamSection from '../_components/dia/DreamSection';
 import ImagineSection from '../_components/dia/ImagineSection';
 import ActSection from '../_components/dia/ActSection';
-import DIAOutroSection from '../_components/dia/DiaOutroSection';
 
 const Dia = () => {
   return (
